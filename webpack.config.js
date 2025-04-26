5e76bdb46822794eaa7770b02f4546151a9c395e
Эрик <crambain.com@gmail.com>
2025-04-26 12:28:05 +0500

    init:first commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const config = {
 		etnomofagi: './src/js/etnomofagi.js',
 		novosti: './src/js/novosti.js',
 		contacts: './src/js/contacts.js',
-		aboutus: './src/js/aboutus.js',
+		aboutus: './src/js/aboutus.js'
 	},
 	output: {
 		filename: '[name].bundle.js',
